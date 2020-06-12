98d122b147ba7ddaf5ab474bc7ab01ff78c5ff33
wexeee <william.clarke@ndcn.ox.ac.uk>
2020-06-12 22:14:27 +0100

    Update test for fsl_mrs.

--- fsl_mrs/tests/test_scripts_fsl_mrs.py
+++ fsl_mrs/tests/test_scripts_fsl_mrs.py
@@ -2,31 +2,31 @@
 
 # Imports
 import subprocess
-import pytest
 import os.path as op
 
 # Files
 testsPath = op.dirname(__file__)
-data = {'metab':op.join(testsPath,'testdata/fsl_mrs/metab.nii'),
-        'water':op.join(testsPath,'testdata/fsl_mrs/water.nii'),
-        'basis':op.join(testsPath,'testdata/fsl_mrs/steam_basis')}
+data = {'metab': op.join(testsPath, 'testdata/fsl_mrs/metab.nii'),
+        'water': op.join(testsPath, 'testdata/fsl_mrs/water.nii'),
+        'basis': op.join(testsPath, 'testdata/fsl_mrs/steam_basis')}
+
 
 def test_fsl_mrs(tmp_path):
 
-    retcode = subprocess.check_call(['fsl_mrs',
-                                    '--data', data['metab'], 
-                                    '--basis', data['basis'], 
-                                    '--output', tmp_path, 
-                                    '--h2o', data['water'], 
-                                    '--TE', '11', 
-                                    '--tissue_frac', '0.45', '0.45', '0.1',
-                                    '--overwrite',
-                                    '--combine', 'Cr', 'PCr',
-                                    '--report'])
+    subprocess.check_call(['fsl_mrs',
+                           '--data', data['metab'],
+                           '--basis', data['basis'],
+                           '--output', tmp_path,
+                           '--h2o', data['water'],
+                           '--TE', '11',
+                           '--tissue_frac', '0.45', '0.45', '0.1',
+                           '--overwrite',
+                           '--combine', 'Cr', 'PCr',
+                           '--report'])
 
-
-    assert op.exists(op.join(tmp_path,'report.html'))
-    assert op.exists(op.join(tmp_path,'all_parameters.csv'))
-    assert op.exists(op.join(tmp_path,'qc.csv'))
-    assert op.exists(op.join(tmp_path,'results_table.csv'))
-    assert op.exists(op.join(tmp_path,'options.txt'))
+    assert op.exists(op.join(tmp_path, 'report.html'))
+    assert op.exists(op.join(tmp_path, 'summary.csv'))
+    assert op.exists(op.join(tmp_path, 'concentrations.csv'))
+    assert op.exists(op.join(tmp_path, 'qc.csv'))
+    assert op.exists(op.join(tmp_path, 'all_parameters.csv'))
+    assert op.exists(op.join(tmp_path, 'options.txt'))
