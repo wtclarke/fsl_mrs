ed7f68f2f554ebc1c2faec0b77be509ae4e08849
wexeee <william.clarke@ndcn.ox.ac.uk>
2020-06-13 18:09:52 +0100

    Preproc test and flake8.

--- fsl_mrs/tests/test_scripts_preproc.py
+++ fsl_mrs/tests/test_scripts_preproc.py
@@ -1,31 +1,30 @@
 import subprocess
-import pytest
-import os.path as op
+from pathlib import Path
 from glob import glob
 
-testsPath = op.dirname(__file__)
-data = {'metab':op.join(testsPath,'testdata/fsl_mrs_preproc/steam_metab_raw*.nii.gz'),
-        'wrefc':op.join(testsPath,'testdata/fsl_mrs_preproc/steam_wref_comb_raw*.nii.gz'),
-        'wrefq':op.join(testsPath,'testdata/fsl_mrs_preproc/steam_wref_quant_raw*.nii.gz'),
-        'ecc':op.join(testsPath,'testdata/fsl_mrs_preproc/steam_ecc_raw*.nii.gz')}
+testsPath = Path(__file__).parent
+data = testsPath / 'testdata/fsl_mrs_preproc'
+
 
 def test_preproc(tmp_path):
 
-    allfiles_metab = glob(data['metab'])
-    allfiles_wrefc = glob(data['wrefc'])
-    allfiles_wrefq = glob(data['wrefq'])
-    allfiles_ecc = glob(data['ecc'])
+    allfiles_metab = list(data.glob('steam_metab_raw*.nii.gz'))
+    allfiles_wrefc = list(data.glob('steam_wref_comb_raw*.nii.gz'))
+    allfiles_wrefq = list(data.glob('steam_wref_quant_raw*.nii.gz'))
+    allfiles_ecc = list(data.glob('steam_ecc_raw*.nii.gz'))
 
-    retcode = subprocess.check_call(['fsl_mrs_preproc',
-                        '--output', str(tmp_path),
-                        '--data']+ allfiles_metab+
-                        ['--reference',]+allfiles_wrefc+
-                        ['--quant',]+allfiles_wrefq+
-                        ['--ecc',]+allfiles_ecc+
-                        ['--hlsvd',
-                        '--leftshift','1',
-                         '--overwrite',
-                        '--report' ])
-    print(retcode)
-    assert retcode==0
-    assert op.isfile(op.join(tmp_path,'mergedReports.html'))
+    retcode = subprocess.check_call(
+            ['fsl_mrs_preproc',
+             '--output', str(tmp_path),
+             '--data'] + allfiles_metab +
+            ['--reference'] + allfiles_wrefc +
+            ['--quant'] + allfiles_wrefq +
+            ['--ecc'] + allfiles_ecc +
+            ['--hlsvd',
+             '--leftshift', '1',
+             '--overwrite',
+             '--report',
+             '--verbose'])
+
+    assert retcode == 0
+    assert (tmp_path / 'mergedReports.html').exists()
