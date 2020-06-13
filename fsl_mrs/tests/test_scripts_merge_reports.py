72d86035afc90cc9140cb447fa869694e9a89d68
wexeee <william.clarke@ndcn.ox.ac.uk>
2020-06-13 16:58:30 +0100

    Merge report script test.

---
+++ fsl_mrs/tests/test_scripts_merge_reports.py
@@ -0,0 +1,26 @@
+# Test the merge report script
+
+# Imports
+import subprocess
+from pathlib import Path
+import shutil
+
+# Files
+testsPath = Path(__file__).parent
+report_dir = testsPath / 'testdata/merge_mrs_reports'
+
+
+def test_merge_mrs_reports(tmp_path):
+
+    for f in report_dir.glob('*.html'):
+        shutil.copy(f, tmp_path)
+
+    htmlfiles = list(tmp_path.glob('*.html'))
+
+    subprocess.check_call(['merge_mrs_reports',
+                           '-d', 'test',
+                           '-f', 'test.html',
+                           '-o', str(tmp_path),
+                           '--delete'] + htmlfiles)
+
+    assert (tmp_path / 'test.html').exists()
