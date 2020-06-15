76049944a87534ee724aee1682b5bdaf49f63c63
wexeee <william.clarke@ndcn.ox.ac.uk>
2020-06-15 09:35:32 +0100

    fsl_mrsi tests.

---
+++ fsl_mrs/tests/test_scripts_fsl_mrsi.py
@@ -0,0 +1,39 @@
+# Test the main svs fitting script
+
+# Imports
+import subprocess
+from pathlib import Path
+
+# Files
+testsPath = Path(__file__).parent
+data = {'metab': testsPath / 'testdata/fsl_mrsi/FID_Metab.nii.gz',
+        'water': testsPath / 'testdata/fsl_mrsi/FID_ref.nii.gz',
+        'basis': testsPath / 'testdata/fsl_mrsi/3T_slaser_32vespa_1250.BASIS',
+        'mask': testsPath / 'testdata/fsl_mrsi/small_mask.nii.gz',
+        'seg_wm': testsPath / 'testdata/fsl_mrsi/mrsi_seg_wm.nii.gz',
+        'seg_gm': testsPath / 'testdata/fsl_mrsi/mrsi_seg_gm.nii.gz',
+        'seg_csf': testsPath / 'testdata/fsl_mrsi/mrsi_seg_csf.nii.gz'}
+
+
+def test_fsl_mrsi(tmp_path):
+
+    subprocess.check_call(['fsl_mrsi',
+                           '--data', data['metab'],
+                           '--basis', data['basis'],
+                           '--output', str(tmp_path / 'fit_out'),
+                           '--h2o', data['water'],
+                           '--TE', '30',
+                           '--add_MM',
+                           '--mask', data['mask'],
+                           '--tissue_frac',
+                           data['seg_wm'],
+                           data['seg_gm'],
+                           data['seg_csf'],
+                           '--overwrite',
+                           '--verbose',
+                           '--combine', 'Cr', 'PCr'])
+
+    assert (tmp_path / 'fit_out/fit').exists()
+    assert (tmp_path / 'fit_out/qc').exists()
+    assert (tmp_path / 'fit_out/uncertainties').exists()
+    assert (tmp_path / 'fit_out/concs').exists()
