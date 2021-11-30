fcf4082def9e548f35d7ea57eabe2eb8741f5517
wexeee <william.clarke@ndcn.ox.ac.uk>
2021-11-30 08:59:55 +0000

    Add dynamic alignment preproc_edit.

--- fsl_mrs/tests/test_scripts_preproc_edit.py
+++ fsl_mrs/tests/test_scripts_preproc_edit.py
@@ -7,6 +7,8 @@ Copyright Will Clarke, University of Oxford, 2021'''
 import subprocess
 from pathlib import Path
 
+from fsl_mrs.utils import mrs_io
+
 testsPath = Path(__file__).parent
 data = testsPath / 'testdata/fsl_mrs_preproc_edit'
 t1 = str(testsPath / 'testdata/svs_segment/T1.anat/T1_biascorr.nii.gz')
@@ -43,3 +45,44 @@ def test_preproc(tmp_path):
     assert (tmp_path / 'options.txt').exists()
     assert (tmp_path / 'mergedReports.html').exists()
     assert (tmp_path / 'voxel_location.png').exists()
+
+
+def test_preproc_noavg(tmp_path):
+
+    metab = str(data / 'metab_raw.nii.gz')
+    wrefc = str(data / 'wref_internal.nii.gz')
+    wrefq = str(data / 'wref_quant.nii.gz')
+    ecc = str(data / 'wref_internal.nii.gz')
+
+    retcode = subprocess.check_call(
+        ['fsl_mrs_preproc_edit',
+         '--output', str(tmp_path),
+         '--data', metab,
+         '--reference', wrefc,
+         '--quant', wrefq,
+         '--ecc', ecc,
+         '--t1', t1,
+         '--dynamic_align_ppm', '1.8', '4.2',
+         '--edit_align_ppm', '2.5', '4.2',
+         '--leftshift', '2',
+         '--overwrite',
+         '--report',
+         '--verbose',
+         '--noaverage'])
+
+    assert retcode == 0
+    assert (tmp_path / 'diff.nii.gz').exists()
+    assert (tmp_path / 'edit_0.nii.gz').exists()
+    assert (tmp_path / 'edit_1.nii.gz').exists()
+    assert (tmp_path / 'wref.nii.gz').exists()
+    assert (tmp_path / 'options.txt').exists()
+    assert (tmp_path / 'mergedReports.html').exists()
+    assert (tmp_path / 'voxel_location.png').exists()
+
+    diff = mrs_io.read_FID(tmp_path / 'diff.nii.gz')
+    on = mrs_io.read_FID(tmp_path / 'edit_0.nii.gz')
+    off = mrs_io.read_FID(tmp_path / 'edit_1.nii.gz')
+
+    assert diff.shape[-1] == 16
+    assert on.shape[-1] == 16
+    assert off.shape[-1] == 16
