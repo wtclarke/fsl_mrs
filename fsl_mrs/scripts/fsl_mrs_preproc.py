5e3707a0e8bba99309837d4185657a9905ff32ed
Vasilis Karlaftis <vasilis.karlaftis@ndcn.ox.ac.uk>
2026-09-04 11:10:45 +0100

    added tests for old molality argument

--- fsl_mrs/scripts/fsl_mrs_preproc.py
+++ fsl_mrs/scripts/fsl_mrs_preproc.py
@@ -139,7 +139,6 @@ def main():
     # Save chosen arguments
     with open(op.join(args.output, "options.txt"), "w") as f:
         # write software version
-        from fsl_mrs import __version__
         f.write(f"FSL-MRS version {__version__}")
         f.write("\n--------\n")
         # write arguments
