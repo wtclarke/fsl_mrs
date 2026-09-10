bug fix on molarity calculation based on H2O_MOLARITY instead of H2O_MOLALITY

fsl_mrs/tests/test_utils_quantify.py
@@ -45,11 +45,11 @@ def test_QuantificationInfo():
     qci = quant.QuantificationInfo(0.000, 40, ['Cr', 'NAA'], 298)
     assert isinstance(qci.summary_table, pd.DataFrame)
     assert qci.relax_corr_water_molal > 55500
-    assert qci.relax_corr_water_molar > 55500
+    assert qci.relax_corr_water_molar > 55000
 
     qci = quant.QuantificationInfo(0.000, 40, ['Cr', 'NAA'], 127)
     assert qci.relax_corr_water_molal > 55500
-    assert qci.relax_corr_water_molar > 55500
+    assert qci.relax_corr_water_molar > 55000
 
     qci = quant.QuantificationInfo(0.010, 3, ['Cr', 'NAA'], 127)
     t2s = STANDARD_T2['3T']
@@ -166,7 +166,7 @@ def test_molefraction_calc():
     qci = quant.QuantificationInfo(0.010, 3, ['NAA'], 298)
     qci.set_fractions({'GM': 0.45, 'WM': 0.40, 'CSF': 0.15})
 
-    # Densitites are 'GM': 0.78, 'WM': 0.65, 'CSF': 0.97
+    # Densities are 'GM': 0.78, 'WM': 0.65, 'CSF': 0.97
     sum_frac = (0.45 * 0.78 + 0.40 * 0.65 + 0.15 * 0.97)
     assert np.isclose(qci.f_GM_H2O, 0.45 * 0.78 / sum_frac)
     assert np.isclose(qci.f_WM_H2O, 0.40 * 0.65 / sum_frac)
@@ -183,7 +183,7 @@ def test_corrected_water_conc():
     # Molality should be close to pure water as density term cancels
     assert np.isclose(qci.relax_corr_water_molal, 55510)
     # Molarity should be scaled by density term as volume fixed
-    assert np.isclose(qci.relax_corr_water_molar, 55510 * 0.78)
+    assert np.isclose(qci.relax_corr_water_molar, 55010 * 0.78)
 
     qci.set_fractions({'GM': 0.50, 'WM': 0.5, 'CSF': 0.0})
 
@@ -192,7 +192,7 @@ def test_corrected_water_conc():
     # Molality should be close to pure water as density term cancels
     assert np.isclose(qci.relax_corr_water_molal, 55510)
     # Molarity should be scaled by density terms as volume fixed
-    assert np.isclose(qci.relax_corr_water_molar, 55510 * (0.78 + 0.65) / 2)
+    assert np.isclose(qci.relax_corr_water_molar, 55010 * (0.78 + 0.65) / 2)
 
     qci = quant.QuantificationInfo(1E-10, 1, ['NAA'], 298)
     qci.set_fractions({'GM': 0.50, 'WM': 0.5, 'CSF': 0.0})
@@ -204,7 +204,7 @@ def test_corrected_water_conc():
     mf_wm = 0.5 * 0.65 / (0.5 * 0.78 + 0.5 * 0.65)
     assert np.isclose(qci.relax_corr_water_molal, 55510 * (qci.R_H2O_GM * mf_gm + qci.R_H2O_WM * mf_wm))
     # Molarity should be scaled by density terms * relaxation terms
-    assert np.isclose(qci.relax_corr_water_molar, 55510 * (0.78 * qci.R_H2O_GM + 0.65 * qci.R_H2O_WM) / 2)
+    assert np.isclose(qci.relax_corr_water_molar, 55010 * (0.78 * qci.R_H2O_GM + 0.65 * qci.R_H2O_WM) / 2)
 
 
 def test_quantifyWater():
@@ -253,13 +253,13 @@ def test_quantifyWater():
     print(res.getConc(scaling='molality'))
     print(res.getConc(scaling='molarity'))
 
-    old_molal_scaling = 1 / (tissueFractions['GM'] * 0.78 +
+    old_molal_scaling = 55.51E3 / 55.01E3 / (tissueFractions['GM'] * 0.78 +
                              tissueFractions['WM'] * 0.65 +
                              tissueFractions['CSF'] * 0.97)
     new_molal_scaling = old_molal_scaling * (1 - tissueFractions['CSF']) / \
         (1 - tissueFractions['CSF'] * 0.97 * old_molal_scaling)
 
     assert np.allclose(res.getConc(scaling='internal'), 1.0)
-    assert np.allclose(res.getConc(scaling='molarity'), 12.77, atol=3E-1)
-    assert np.allclose(res.getConc(scaling='molality'), 12.77 * new_molal_scaling, atol=3E-1)
-    assert np.allclose(res.getConc(scaling='old_WRONG_molality'), 12.77 * old_molal_scaling, atol=3E-1)
+    assert np.allclose(res.getConc(scaling='molarity'), 12.65, atol=3E-1)
+    assert np.allclose(res.getConc(scaling='molality'), 12.65 * new_molal_scaling, atol=3E-1)
+    assert np.allclose(res.getConc(scaling='old_WRONG_molality'), 12.65 * old_molal_scaling, atol=3E-1)

fsl_mrs/utils/quantify.py
@@ -13,7 +13,7 @@ from scipy.optimize import minimize
 import pandas as pd
 
 from fsl_mrs.utils.misc import FIDToSpec, checkCFUnits
-from fsl_mrs.utils.constants import H2O_MOLALITY, TISSUE_WATER_DENSITY, \
+from fsl_mrs.utils.constants import H2O_MOLALITY, H2O_MOLARITY, TISSUE_WATER_DENSITY, \
     STANDARD_T1, STANDARD_T2, GYRO_MAG_RATIO, \
     H2O_PROTONS, WATER_SCALING_METAB, \
     WATER_SCALING_METAB_PROTONS, \
@@ -534,9 +534,9 @@ class QuantificationInfo(object):
         :rtype: float
         """
         if self._fractions is None:
-            return self.R_H2O * H2O_MOLALITY
+            return self.R_H2O * H2O_MOLARITY
         else:
-            return H2O_MOLALITY * (self.f_GM * self.d_GM * self.R_H2O_GM
+            return H2O_MOLARITY * (self.f_GM * self.d_GM * self.R_H2O_GM
                                    + self.f_WM * self.d_WM * self.R_H2O_WM
                                    + self.f_CSF * self.d_CSF * self.R_H2O_CSF)
 
@@ -667,16 +667,18 @@ def quantifyWater(mrs, results, quant_info, verbose=False):
     SMObs = mref.integral
 
     # Calculate concentration scalings
-    # EQ 4 and 6 in https://doi.org/10.1002/nbm.4257
+    # EQ 4 in https://doi.org/10.1002/nbm.4257
     # conc_molal =  (SMObs *(Q.f_GM_H20*Q.R_H2O_GM + Q.f_WM_H20*Q.R_H2O_WM + Q.f_CSF_H20*Q.R_H2O_CSF)\
     #                       / (SH2OObs*(1-Q.f_CSF_H20)*Q.R_M)) \
     #                 * (H2O_PROTONS/refProtons)\
     #                 * H2O_MOLALITY
 
+    # EQ A21 from https://doi.org/10.1002/nbm.4257 SI & https://doi.org/10.1002/nbm.3914
+    # (notice the difference from EQ 6 of the main manuscript that uses H2O_MOLALITY instead of H2O_MOLARITY)
     # conc_molar =  (SMObs *(Q.f_GM*Q.d_GM*Q.R_H2O_GM + Q.f_WM*Q.d_WM*Q.R_H2O_WM + Q.f_CSF*Q.d_CSF*Q.R_H2O_CSF)\
     #                       / (SH2OObs*(1-Q.f_CSF)*Q.R_M))\
     #                 * (H2O_PROTONS/refProtons)\
-    #                 * H2O_MOLALITY
+    #                 * H2O_MOLARITY
 
     # Note the difference between Q.f_X and Q.f_X_H2O. Equation 5 of reference. With thanks to Alex Craig-Craven
     # for pointing this out.